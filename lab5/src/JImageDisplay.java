c19a48510f35c870a61e6cfa4c589552bb9ba9dc
benbeardsmith <shi_foo@mail.ru>
2020-06-11 12:10:40 +0300

    Create JImageDisplay.java

---
+++ lab5/src/JImageDisplay.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.image.*;
+import java.awt.*;
+
+public class JImageDisplay extends JComponent{
+    BufferedImage image;
+
+    public JImageDisplay(int w, int h) {
+        image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);//задаем размер
+        Dimension imageDimension = new Dimension(w, h); // создаем
+        super.setPreferredSize(imageDimension); //отображение
+    }
+
+    //отрисовка
+    public void paintComponent (Graphics g){
+        super.paintComponent(g);
+        g.drawImage (image, 0, 0, image.getWidth(), image.getHeight(), null);
+    }
+
+    //очистка
+    public void clearImage(){
+        for (int x=0;x<image.getWidth();x++)
+            for (int y=0;y<image.getHeight();y++)
+                image.setRGB(x,y,0);
+    }
+
+    //закрашивание 1 пикселя
+    public void drawPixel(int x, int y, int rgbColor){
+        image.setRGB(x,y,rgbColor);
+    }
+}
